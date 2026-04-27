83d340bde0c866eff8b926b6923d00e03a1c1e71
YangQuan <qyang@memect.co>
2026-04-27 16:03:33 +0800

    完善下载

--- src/memect/models/__init__.py
+++ src/memect/models/__init__.py
@@ -94,6 +94,20 @@ def download_all():
 
 def download_latex():
     from rapid_latex_ocr import LatexOCR
+    from pathlib import Path
+    from rapid_latex_ocr.utils import DownloadModel
+    from rapid_latex_ocr import utils
+    def patch_url():
+        #old_init=DownloadModel.__init__
+        def new_init(self) -> None:
+            #这个太慢
+            #self.url = "https://github.com/RapidAI/RapidLaTeXOCR/releases/download/v0.0.0"
+            self.url = "https://modelscope.cn/models/Memect/rapid_latex_ocr/resolve/v1.0.0"
+            self.cur_dir = Path(utils.__file__).resolve().parent
+        DownloadModel.__init__=new_init
+
+    patch_url()
+
     LatexOCR()
 
 def download_ocr():
--- src/memect/pdf/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import patch_rapid_latex_ocr
